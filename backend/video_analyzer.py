c894920bad9663e4298e50288749d4d8fb2fa7cd
SudoAttack <wayne8798@gmail.com>
2014-03-28 22:53:37 -0500

    enhance video analyzer

--- backend/video_analyzer.py
+++ backend/video_analyzer.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python
 
-import cv2, sys
+import cv2, sys, operator
+from PIL import Image
+from collections import defaultdict
 
-fname = sys.argv[1]
+def find_primary_colors(count):
+    im = Image.open("frame%d.jpg" % count)
+    by_color = defaultdict(int)
+    for pixel in im.getdata():
+        by_color[pixel] += 1
+    sorted_colors = sorted(by_color.iteritems(), key=operator.itemgetter(1))
+    print "Frame " + str(count)
+    print sorted_colors[-1]
+    print sorted_colors[-2]
+    print sorted_colors[-3]
+    print sorted_colors[-4]
+    print sorted_colors[-5]
+
+interval = int(sys.argv[1])
+fname = sys.argv[2]
 
 vidcap = cv2.VideoCapture(fname)
-success, image = vidcap.read()
+frame_rate = vidcap.get(5)
 count = 0
+frame_count = 0
+success = True
+
 while success:
     success, image = vidcap.read()
-    cv2.imwrite("frame%d.jpg" % count, image)
-    count += 1
-    if count > 100:
-        break
+    if frame_count % (int(frame_rate * interval)) == 0:
+        cv2.imwrite("frame%d.jpg" % count, image)
+        find_primary_colors(count)
+        count += 1
+    frame_count += 1
