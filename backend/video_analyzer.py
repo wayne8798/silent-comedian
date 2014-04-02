6f137c7f54269191daf159b684b6886668b908e2
fong11 <aznknite@gmail.com>
2014-04-02 01:33:35 -0500

    Update video_analyzer.py

--- backend/video_analyzer.py
+++ backend/video_analyzer.py
@@ -1,46 +1,72 @@
 #!/usr/bin/python
 
-import cv2, sys, operator
+import cv2, sys, operator,os
 from PIL import Image
 from collections import defaultdict
 
+totalTime = 0
+totalPix = 0
+color_array = []
+ratio_array = []
+db_array = []
+moviename = "thering.mp4"
 # function that finds the primary colors
 def find_primary_colors(count):
-    im = Image.open("frame%d.jpg" % count)
+    im = Image.open(newdir +"/frame%d.jpg" % count)
+    (width, height) = im.size
+    totalPix = width * height
     by_color = defaultdict(int)
     for pixel in im.getdata():
         by_color[pixel] += 1
     sorted_colors = sorted(by_color.iteritems(), key=operator.itemgetter(1))
     # print out top 5 most frequent colors in the count'th frame.
-    print "Frame " + str(count)
-    print sorted_colors[-1]
+   # print>>f, "Frame " + str(count)
+    print>>f, sorted_colors[-1][0]
+    print>>f2, float(sorted_colors[-1][1])/totalPix
+    if len(sorted_colors) == 1:
+        print>>f, sorted_colors[-1][0]
+        print>>f2, float(sorted_colors[-1][1])/totalPix
+        print>>f, sorted_colors[-1][0]
+        print>>f2, float(sorted_colors[-1][1])/totalPix
+        print>>f, sorted_colors[-1][0]
+        print>>f2, float(sorted_colors[-1][1])/totalPix
+        print>>f, sorted_colors[-1][0]
+        print>>f2, float(sorted_colors[-1][1])/totalPix
     if len(sorted_colors) > 1:
-        print sorted_colors[-2]
+        print>>f, sorted_colors[-2][0]
+        print>>f2, float(sorted_colors[-2][1])/totalPix
     if len(sorted_colors) > 2:
-        print sorted_colors[-3]
+        print>>f, sorted_colors[-3][0]
+        print>>f2, float(sorted_colors[-3][1])/totalPix
     if len(sorted_colors) > 3:
-        print sorted_colors[-4]
+        print>>f, sorted_colors[-4][0]
+        print>>f2, float(sorted_colors[-4][1])/totalPix
     if len(sorted_colors) > 4:
-        print sorted_colors[-5]
+        print>>f, sorted_colors[-5][0]
+        print>>f2, float(sorted_colors[-5][1])/totalPix
 
 # takes in two arguments. first is the length of interval in seconds.
 # for example, if interval = 60, then the script will extract a frame
 # every 60 seconds and find the primary 5 colors in that frame.
 # the second argument is the name of the mp4 file.
-interval = int(sys.argv[1])
-fname = sys.argv[2]
-
-vidcap = cv2.VideoCapture(fname)
+interval = int(30)
+#moviename = "thering.mp4"
+newdir = moviename.replace(".mp4", "")
+vidcap = cv2.VideoCapture(moviename)
+f2 = open("ratios.txt", 'w')
+f = open("colors.txt", 'w')
 # number of frame per second. please note it can be a float number.
 frame_rate = vidcap.get(5)
 count = 0
 frame_count = 0
 success = True
-
+if not os.path.exists(newdir):
+    os.makedirs(newdir)
 while success:
     success, image = vidcap.read()
     if frame_count % (int(frame_rate * interval)) == 0:
-        cv2.imwrite("frame%d.jpg" % count, image)
+       # cv2.imwrite(newdir + "/frame%d.jpg" % count, image)
         find_primary_colors(count)
         count += 1
     frame_count += 1
+f.close()
